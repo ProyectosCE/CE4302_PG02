34548770e15567d72147ed2bfd5eeb560c139767
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2026-05-25 21:54:25 -0600

    [ADD] Enhance build and run scripts for scalar and SIMD implementations; add performance analysis script

--- Codigo Fuente/run.sh
+++ Codigo Fuente/run.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "Usage: ./run.sh <dataset>"
+# VALIDACION DE ARGUMENTOS
+if [ $# -lt 2 ]; then
+    echo "Usage: ./run.sh <implementation> <dataset>"
+    echo ""
+    echo "Implementations:"
+    echo "  scalar"
+    echo "  simd"
+    echo ""
+    echo "Datasets:"
+    echo "  small"
+    echo "  medium"
+    echo "  large"
+    exit 1
+fi
+
+IMPLEMENTATION=$1
+DATASET=$2
+
+# VALIDACION IMPLEMENTACION
+if [ "$IMPLEMENTATION" != "scalar" ] && \
+   [ "$IMPLEMENTATION" != "simd" ]; then
+
+    echo "Invalid implementation: $IMPLEMENTATION"
+    echo "Available implementations: scalar, simd"
+    exit 1
+fi
+
+# VALIDACION DATASET
+if [ "$DATASET" != "small" ] && \
+   [ "$DATASET" != "medium" ] && \
+   [ "$DATASET" != "large" ]; then
+
+    echo "Invalid dataset: $DATASET"
     echo "Available datasets: small, medium, large"
     exit 1
 fi
 
+# BUILD
 make clean
-make
+make $IMPLEMENTATION
 
+# RUN
 echo ""
 echo "Running FIR Project..."
+echo "Implementation : $IMPLEMENTATION"
+echo "Dataset        : $DATASET"
 echo ""
 
-./build/fir_project $1
+./build/fir_project $DATASET
